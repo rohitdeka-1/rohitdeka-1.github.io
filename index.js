90c98109720d2ba9fde1a0c17cc63bf300896a03
Rohit Deka <rohit.23MIM10093@vitbhopal.ac.in>
2024-12-18 11:03:09 +0530

    www.rohitdeka.me

--- index.js
+++ index.js
@@ -1,45 +1,11 @@
-let count = 0;
-let highestScore = 0;
-
+let count = 1;
 const message = document.getElementById('message');
-const highscoreDisplay = document.getElementById('highscore');
 
-window.onload = async function() {
-    try {
-        const response = await fetch('/highscore');
-        const data = await response.json();
-        highestScore = data.highscore;
-        highscoreDisplay.textContent = `Highest Score: ${highestScore}`;
-    } catch (error) {
-        console.error('Error fetching highscore:', error);
-    }
-}
-
-function displayMessage() {
-    message.textContent = `Clicks: ${count}`;
-    count += 1;
-
-    if (count > highestScore) {
-        highestScore = count;
-        updateHighscore(highestScore);
-    }
-}
-
-
-async function updateHighscore(newHighscore) {
-    try {
-        const response = await fetch('https://rohit-deka-rhd-e435a3203b63.herokuapp.com/', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ highscore: newHighscore })
-        });
-
-        const data = await response.json();
-        console.log('Highscore updated:', data);
-        highscoreDisplay.textContent = `Highest Score: ${data.highscore}`;
-    } catch (error) {
-        console.error('Error updating highscore:', error);
-    }
-}
+$(document).ready(function() {
+    // Handle button click event
+    $(".myButton").on("click", function() {
+        message.textContent = `Clicks: ${count}`;
+        count += 1;
+        console.log(count);
+    });
+});
