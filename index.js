9d8347f5d9c141928d47527bc6d7e566c9848be3
Rohit Deka <rohit.23MIM10093@vitbhopal.ac.in>
2024-12-18 12:31:51 +0530

    rohitdeka.me

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ let count = 1;
 const message = document.getElementById('message');
 
 $(document).ready(function() {
-    // Handle button click event
+    if($(".reset").on("click",function(){
+        count = 0;
+        message.textContent = `Clicks: ${count}`;
+        count = 1;
+    }))
     $(".myButton").on("click", function() {
         message.textContent = `Clicks: ${count}`;
         count += 1;
         console.log(count);
     });
 });
+
+
