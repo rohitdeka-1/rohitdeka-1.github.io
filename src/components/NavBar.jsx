c5e3eeaee2e42fbc84024cfd956a0b6c11a0473b
Rohit Deka <studyrhd@gmail.com>
2025-03-31 10:21:54 +0530

    drop blur

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -11,15 +11,14 @@ const NavItem = ({ icon }) => {
 const NavBar = () => {
   return (
     <div className="fixed inset-x-0 bottom-4 flex justify-center z-[8888] pointer-events-none">
-      <div className="flex items-center justify-around w-64 h-14 2xl:h-16 bg-gray-900/90 backdrop-blur-md text-white rounded-full shadow-lg border border-gray-700 pointer-events-auto">
-        <NavItem icon={<Home size={22} />} />
-        <NavItem icon={<User size={22} />} />
-        <NavItem icon={<Folder size={22} />} />
-        <NavItem icon={<Mail size={22} />} />
+      <div className="flex items-center justify-around w-64 md:w-80 lg:w-90 lg:px-10 xl:w-[29rem] 2xl:w-[30rem] h-14 2xl:h-15  backdrop-blur-md text-white rounded-full shadow-lg border border-gray-700 pointer-events-auto">
+        <NavItem icon={<Home size={24} />} />
+        <NavItem icon={<User size={24} />} />
+        <NavItem icon={<Folder size={24} />} />
+        <NavItem icon={<Mail size={24} />} />
       </div>
     </div>
   )
 }
 
 export default NavBar
-
